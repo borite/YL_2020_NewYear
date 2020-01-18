472b357f273b8cd10a52cd3766babcd5accb1e39
David Zheng <nmdigboy@hotmail.com>
2020-01-18 15:09:36 +0800

    更新游戏结束时的处理逻辑

--- js/raward.js
+++ js/raward.js
@@ -2,17 +2,87 @@
 
 
 //游戏结束Handle
-//true胜利，false失败
+//e---true胜利，false失败
 //gcode  1-贴春联 2-糖葫芦 3-点爆竹 4-贺春节 5-挂灯笼
 function selRaw(e,gcode){
 	$("#end").css({"display":"block"});//显示结束页面
 	timeOpen=false;//计时器停止
     if(e==true && gcode>0){
+		//getCard(gcode);
 		
-		getCard(gcode);
+	var userInfo=localStorage.getItem('userInfo');
+	var a=JSON.parse(userInfo);
+	console.log(JSON.parse(userInfo));
+	var d=new Date();
+	//uOpenID--用户微信OpenID  g
+	//1-贴春联 2-糖葫芦 3-点爆竹 4-贺春节 5-挂灯笼
+    $.get("https://customer.imotstudio.net/yl2020NewYear/api/Game/ChouCard",{
+		openID:a.Data.OpenID,
+		GameType:gcode,
+		t:d.getTime()
+	}).done(function(res){
+		console.log(res);
+		var c=res.Code;
+		if(c==200){
+			var cardCode="";
+			var cname=res.Data;
+			switch(cname){
+				case "鼠你强":
+					cardCode="3";
+					break;
+				case "属你富":
+					cardCode="1";
+					break;
+				case "属你美":
+					cardCode="2";
+					break;	
+				case "属你乐":
+					cardCode="4";
+					break;	
+				case "鼠你甜":
+					cardCode="5";
+					break;
+				case "尊重卡":
+					cardCode="6";
+					break;
+				default:
+					break;
+				
+			}
+			if(cardCode==""){
+				return false;
+			}
+			getRaw(cardCode);
+		}
+		
+		if(c==204){
+			$("#end").html('<div class="lw100 lh100 lpos_fix lbg_co_black" style="opacity: 0.5;top: 0;"></div>\
+				<div class="lpos_fix lw100 ltex_cen" style="top: 6%;line-height: 60px;">\
+					<img src="img/goodluck.png" alt="好运社" width="30%"/>\
+					<br/>\
+					<img src="img/win.png" alt="" width="40%"/>\
+					<br/>\
+					<img src="img/wintext.png" alt="" width="70%"/>\
+					<br/>\
+					<img onClick="reStart()" src="img/restart.png" alt="重新开始游戏" width="50%"/>\
+				</div>\
+				<header class="lw100 ldis_fle lpos_fix l10" style="height: 10%;justify-content: space-between;padding: 3% 0;top:0">\
+				<a href="index.html" class="ldis_blo lbg_co_2 ltex_cen lpos_rel" style="width: 20%;height: 78%;border: 6px #bdae9a solid; border-radius: 6px;margin-left: 3%;">\
+					<img src="img/back.png" class="ldis_blo lpos_abs" style="right: 50%;top: 50%;margin-right: -25px;margin-top: -21px;" alt="返回" width="50px" height="42px"/>\
+				</a>\
+				</header>');
+		}
+		
+		if(c==404){
+			selRaw(true,gcode);
+		}
+	});
+		
+		
+		
+		
+		
 		//$.post('https://customer.imotstudio.net/yl2020NewYear/api/Game/ChouCard')
-		
-		
 		/*vice(0);
 		if(判断当日抽卡次数){//当单日抽卡次数达到上限时，胜利界面
 		   $("#end").html('<div class="lw100 lh100 lpos_fix lbg_co_black" style="opacity: 0.5;top: 0;"></div>\
@@ -128,24 +198,3 @@ function vice(nub){
 	
 }
 
-
-
-//抽奖
-//uOpenID--用户微信OpenID  g
-//ameCode--游戏代码 
-//1-贴春联 2-糖葫芦 3-点爆竹 4-贺春节 5-挂灯笼
-function getCard(gameCode){
-	
-	var userInfo=localStorage.getItem('userInfo');
-	var a=JSON.parse(userInfo);
-	console.log(JSON.parse(userInfo));
-	var d=new Date();
-	
-    $.get("https://customer.imotstudio.net/yl2020NewYear/api/Game/ChouCard",{
-		openID:a.Data.OpenID,
-		GameType:gameCode,
-		t:d.getTime()
-	}).done(function(res){
-		console.log(res);
-	})
-}
